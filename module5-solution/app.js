d8347eb9c53995bb221d35d950951a3687b6a5ca
pratapsingh24 <101635137+pratapsingh24@users.noreply.github.com>
2023-07-26 19:24:10 +0000

    fifth

--- module5-solution/app.js
+++ module5-solution/app.js
@@ -25,15 +25,40 @@ angular.module('RestaurantApp', ['ngRoute'])
       $location.path('/myinfo');
     };
   }])
-  .controller('SignUpController', ['$http', function($http) {
+  .controller('SignUpController', ['$http', 'MenuDataService', function($http, MenuDataService) {
     var signUpCtrl = this;
 
+    signUpCtrl.form = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      favoriteDish: ''
+    };
+
+    signUpCtrl.dishExists = true;
+
     signUpCtrl.submitForm = function() {
-      // Implement form submission and validation here
-      // Save user's preference using a service
+      // Form validation
+      if (signUpCtrl.signupForm.$invalid) {
+        return;
+      }
+
+      // Check if the favorite dish exists
+      MenuDataService.getMenuItem(signUpCtrl.form.favoriteDish)
+        .then(function(response) {
+          if (response === null) {
+            signUpCtrl.dishExists = false;
+          } else {
+            signUpCtrl.dishExists = true;
+            // Save user's preference
+            // Here you can use a service to save the data, but for simplicity, we'll just log the form data
+            console.log(signUpCtrl.form);
+          }
+        });
     };
   }])
-  .controller('MyInfoController', ['$http', function($http) {
+  .controller('MyInfoController', ['MenuDataService', function(MenuDataService) {
     var myInfoCtrl = this;
 
     myInfoCtrl.userInfo = {
@@ -44,5 +69,26 @@ angular.module('RestaurantApp', ['ngRoute'])
       favoriteDish: 'L1'
     };
 
-    // Retrieve favorite dish using $http
+    // Retrieve favorite dish using the MenuDataService
+    MenuDataService.getMenuItem(myInfoCtrl.userInfo.favoriteDish)
+      .then(function(response) {
+        if (response !== null) {
+          myInfoCtrl.favoriteDish = response;
+        }
+      });
+  }])
+  .service('MenuDataService', ['$http', function($http) {
+    var service = this;
+
+    service.getMenuItem = function(menuNumber) {
+      return $http.get('https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/' + menuNumber + '.json')
+        .then(function(response) {
+          return response.data;
+        })
+        .catch(function(error) {
+          // Handle error if necessary
+          console.log('Error fetching menu item:', error);
+          return null;
+        });
+    };
   }]);
