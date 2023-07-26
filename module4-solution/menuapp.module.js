733e96d866c075f8f5a1bd7676b749a8c9e73768
pratapsingh24 <101635137+pratapsingh24@users.noreply.github.com>
2023-07-26 18:33:35 +0000

    forth

--- module4-solution/menuapp.module.js
+++ module4-solution/menuapp.module.js
@@ -1,6 +1,35 @@
 (function () {
     'use strict';
   
-    angular.module('MenuApp', ['data', 'ui.router']);
+    angular.module('MenuApp', ['data', 'ui.router'])
+      .config(RoutesConfig);
+  
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+  
+      // Redirect to home page if no other URL matches
+      $urlRouterProvider.otherwise('/');
+  
+      // Set up UI states
+      $stateProvider
+  
+        // Home page
+        .state('home', {
+          url: '/',
+          template: '<h1>Welcome to our Restaurant</h1>'
+        })
+  
+        // Categories list page
+        .state('categories', {
+          url: '/categories',
+          template: '<categories></categories>'
+        })
+  
+        // Items list page
+        .state('items', {
+          url: '/items/{categoryShortName}',
+          template: '<items></items>'
+        });
+    }
   })();
   
